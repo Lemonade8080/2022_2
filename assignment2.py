4278fe876874ff3abf8394b7a3e40b6783f112b5
Lemonade8080 <58336426+Lemonade8080@users.noreply.github.com>
2022-10-03 20:44:00 +0900

    Final

--- assignment2.py
+++ assignment2.py
@@ -1,14 +1,15 @@
+# 2018253061_현동윤
+
 import math
 import sys
 import numpy as np
 from copy import deepcopy
 
-
 K = 10
 DATA_COUNT = 500  # GENE 개수
 
-center_list = np.zeros((K, 12), dtype=float)  # 중심 [ 12 개의 디멘션 ]
-cluster_list = np.zeros(DATA_COUNT)           # 군집 [ 500 개의 데이터의 군집 ]
+centroid_list = np.zeros((K, 12), dtype=float)  # 중심 [ 12 개의 디멘션 ]
+cluster_list = np.zeros(DATA_COUNT)             # 군집 [ 500 개의 데이터의 군집 ]
 dataset = []
 
 
@@ -21,12 +22,14 @@ def main(args):
 def assignment2():  # cluster 구하기 -> center 구하기 -> 중심값 변화 구하기
 
     move = True
+    count = 0
     init_center()
 
     while move:
-        old_center_list = deepcopy(center_list)
+        old_centroid_list = deepcopy(centroid_list)
         clustering()
-        move = calc_distance(old_center_list, center_list).any()
+        move = calc_distance(old_centroid_list, centroid_list).any()
+        count += 1
 
 
 def clustering():
@@ -35,29 +38,35 @@ def clustering():
 
 
 def init_center():
+    # rand = random.sample(range(0, 500), 10)
+    # for idx in range(K):
+    #     centroid_list[idx] = dataset[rand[idx]]
+
     init_cluster = [i // 50 for i in range(500)]
 
     for idx in range(K):
-        data_sum = [dataset[cluster_idx] for cluster_idx in range(DATA_COUNT) if init_cluster[cluster_idx] == idx]
-        center_list[idx] = np.round(np.mean(data_sum, axis=0), 3)
+        same_cluster = [dataset[cluster_idx] for cluster_idx in range(DATA_COUNT) if init_cluster[cluster_idx] == idx]
+        centroid_list[idx] = np.round(np.mean(same_cluster, axis=0), 3)
 
 
 def update_clusters():
     for cluster_idx in range(DATA_COUNT):
         distances = np.zeros(K)
-        for j in range(K):
-            distances[j] = calc_distance(dataset[cluster_idx], center_list[j])
+        for centroid_idx in range(K):
+            distances[centroid_idx] = calc_distance(dataset[cluster_idx], centroid_list[centroid_idx])
         cluster_list[cluster_idx] = np.argmin(distances)
 
 
 def update_centers():
     for idx in range(K):
-        data_sum = [dataset[cluster_idx] for cluster_idx in range(DATA_COUNT) if cluster_list[cluster_idx] == idx]
-        center_list[idx] = np.round(np.mean(data_sum, axis=0), 3)
+        same_cluster = [dataset[cluster_idx] for cluster_idx in range(DATA_COUNT) if cluster_list[cluster_idx] == idx]
+        centroid_list[idx] = np.round(np.mean(same_cluster, axis=0), 3)
 
 
 def calc_distance(data, center):  # data 와 center 간의 거리 구하기
-    return np.round(sum([(data - center) ** 2 for data, center in list(zip(data, center))]), 3)
+    dist_list = [(data - center) ** 2 for data, center in list(zip(data, center))]  # 12개 요소 각각의 차이
+    return np.round(sum(dist_list) ** 0.5, 3)
+    #  return np.round(sum([(data - center) ** 2 for data, center in list(zip(data, center))]) ** 0.5, 3)
 
 
 def read_file(args):
@@ -68,21 +77,19 @@ def read_file(args):
 
 
 def write_file():
-    with open("assignment2.txt", "w") as file:
+    with open("assignment2_output.txt", "w") as file:
         file.write(result())
-
     file.close()
 
 
-def result():
-    result_list = [[] for k in range(K)]
+def result():  # 결과 str 출력
     test = ''
-    for i in range(K):
-        for j in range(DATA_COUNT):
-            if cluster_list[j] == i:
-                result_list[i].append(j)
-        test += str(len(result_list[i])) + ': ' + str(result_list[i]) + '\n'
-        print(len(result_list[i]), ': ', result_list[i], '\n')
+    for idx in range(K):
+        data_sum = [cluster_idx for cluster_idx in range(DATA_COUNT) if cluster_list[cluster_idx] == idx]
+        test += str(len(data_sum)) + ': '
+        for data in data_sum:
+            test += str(data) + ' '
+        test += '\n'
 
     return test
 
