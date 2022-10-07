01d1457ca44c27f5fb1544b60cdcaba496592a84
Lemonade8080 <58336426+Lemonade8080@users.noreply.github.com>
2022-10-07 23:12:28 +0900

    K-Medoid

--- assignment3.py
+++ assignment3.py
@@ -1,106 +1,108 @@
 # 2018253061_현동윤
-
-import math
-import sys
 import numpy as np
 import time
-from copy import deepcopy
+import sys
 
 K = 10
 DATA_COUNT = 500  # GENE 개수
 
-centroid_list = np.zeros((K, 12), dtype=float)  # 중심 [ 12 개의 디멘션 ]
-cluster_list = np.zeros(DATA_COUNT)             # 군집 [ 500 개의 데이터의 군집 ]
 dataset = []
+i_j_distance = np.zeros((DATA_COUNT, DATA_COUNT))
 
 
-def main():
-    read_file()
+def main(args):
+    read_file(args)
     assignment3()
-    write_file()
 
 
 def assignment3():
-    start_time = time.process_time_ns() * 1000
-    start_time2 = time.process_time()
+    medoid = init_medoid()
 
-    clustering()
+    start_time = time.process_time() * 1000
+    cluster = clustering(medoid)
+    end_time = time.process_time() * 1000
 
-    end_time = time.process_time_ns() * 1000
-    end_time2 = time.process_time()
-    print('time ', end_time - start_time)
-    print('time ', end_time2 - start_time2)
+    work_time = end_time - start_time
+
+    write_file(cluster, work_time)
+
+    print(result(cluster))                         # Print Clusters
+    print('time : ', work_time, 'ms')  # Print Time
 
 
-def clustering():  # cluster 구하기 -> centroid 구하기 -> 중심값 변화 구하기
-
-    move = True
-    init_centroid()
-    a = 0
+def clustering(medoid, move=True):
+    cluster = np.zeros(DATA_COUNT)
+    count = 0
     while move:
-        old_centroid_list = deepcopy(centroid_list)
-        update_clusters()
-        update_centroid()
-        move = calc_distance(old_centroid_list, centroid_list).any()
-        a += 1
+        old_list = cluster
+        cluster = update_clusters(medoid)
+        medoid = update_medoids(cluster)
+        count += 1
+        move = (old_list != cluster).any()
 
-    print(result())
-    print(centroid_list)
-    print(a)
+    print('\nClustering Count: ', count - 1, '\n')
+
+    return cluster
 
 
-def init_centroid():
-    sum_list = [np.round(np.sum([calc_distance(data, data2) for data2 in dataset]), 3) for data in dataset]
-    idx_of_min = [sum_list.index(np.sort(sum_list)[idx]) for idx in range(K)]
+def init_medoid():  # Medoid 초기화
+
+    for i in range(DATA_COUNT):  # 500개 간의 거리 값 설정
+        i_j_distance[i] = [calc_distance(dataset[i], data) for data in dataset]
+
+    distance = [np.sum([calc_distance(data, data2) for data2 in dataset]) for data in dataset]
+    return [distance.index(np.sort(distance)[idx]) for idx in range(K)]
+
+
+def update_clusters(medoid):
+    cluster = np.zeros(DATA_COUNT)
+
+    for data_idx in range(DATA_COUNT):
+        distance_in_cluster = [calc_distance(dataset[data_idx], dataset[medoid[idx]]) for idx in range(K)]
+        cluster[data_idx] = np.argmin(distance_in_cluster)
+    return cluster
+
+
+def update_medoids(cluster):
+    medoid = np.zeros(K, int)
 
     for idx in range(K):
-        centroid_list[idx] = dataset[idx_of_min[idx]]
-
-    # centroid_list = [dataset[idx] for idx in idx_of_min]
-
-
-def update_centroid():
-    for idx in range(K):
-        cluster = [dataset[cluster_idx] for cluster_idx in range(DATA_COUNT) if cluster_list[cluster_idx] == idx]
-        centroid_list[idx] = min_data(cluster)
+        test = [data_idx for data_idx in range(DATA_COUNT) if cluster[data_idx] == idx]
+        medoid[idx] = get_medoid(test)
+    return medoid
 
 
-def min_data(cluster):
-    sum_list = [np.round(np.sum([calc_distance(data1, data2) for data2 in cluster]), 3) for data1 in cluster]
-    return cluster[np.argmin(sum_list)]
-
-
-def update_clusters():
-    for cluster_idx in range(DATA_COUNT):
-        distances = np.zeros(K)
-        for idx in range(K):
-            distances[idx] = calc_distance(dataset[cluster_idx], centroid_list[idx])
-        cluster_list[cluster_idx] = np.argmin(distances)
+def get_medoid(index_list):  # Medoid 인 data_idx 를 return 한다.
+    sum_in_cluster = [np.sum([i_j_distance[i][j] for j in index_list]) for i in index_list]  # size = (cluster,1)
+    min_idx = np.argmin(sum_in_cluster)
+    data_idx_is_medoid = index_list[min_idx]
+    return data_idx_is_medoid
 
 
 def calc_distance(data, centroid):  # data 와 centroid 간의 거리 구하기
-    dist_list = [(data - centroid) ** 2 for data, centroid in list(zip(data, centroid))]  # 12개 요소 각각의 차이
-    return np.round(sum(dist_list) ** 0.5, 3)
-    #  return np.round(sum([(data - centroid) ** 2 for data, centroid in list(zip(data, centroid))]) ** 0.5, 3)
+    calc_dimension = [(data - centroid) ** 2 for data, centroid in list(zip(data, centroid))]
+    distance = np.round(sum(calc_dimension) ** 0.5, 3)
+    return distance
 
 
-def read_file():
-    file = open('data.txt', 'r')
+def read_file(args):
+    file = open(args, 'r')
     for line in file:
         dataset.append(list(map(float, line.replace('\n', '').split('\t'))))
     file.close()
 
 
-def write_file():
+def write_file(cluster, work_time):
     with open("assignment3_output.txt", "w") as file:
-        file.write(result())
+        file.write(result(cluster))
+        file.write('time : ' + str(work_time) + 'ms')
     file.close()
 
 
-def result():  # 결과 str 출력
+def result(cluster):  # 결과 str 출력
     test = ''
     for idx in range(K):
-        data_sum = [cluster_idx for cluster_idx in range(DATA_COUNT) if cluster_list[cluster_idx] == idx]
+        data_sum = [data_idx for data_idx in range(DATA_COUNT) if cluster[data_idx] == idx]
         test += str(len(data_sum)) + ': '
         for data in data_sum:
             test += str(data) + ' '
@@ -110,5 +112,4 @@ def result():  # 결과 str 출력
 
 
 if __name__ == '__main__':
-    main()
-    # main(sys.argv[1])
+    main(sys.argv[1])
