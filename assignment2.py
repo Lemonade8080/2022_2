b688e420ba94793959aca26dada5fc1991164929
Lemonade8080 <58336426+Lemonade8080@users.noreply.github.com>
2022-10-09 02:44:23 +0900

    K-Means

--- assignment2.py
+++ assignment2.py
@@ -1,6 +1,3 @@
-# 2018253061_현동윤
-
-import math
 import sys
 import numpy as np
 from copy import deepcopy
@@ -22,14 +19,13 @@ def main(args):
 def assignment2():  # cluster 구하기 -> center 구하기 -> 중심값 변화 구하기
 
     move = True
-    count = 0
     init_center()
 
     while move:
         old_centroid_list = deepcopy(centroid_list)
-        clustering()
+        update_clusters()
+        update_centers()
         move = calc_distance(old_centroid_list, centroid_list).any()
-        count += 1
 
 
 def clustering():
@@ -50,23 +46,19 @@ def init_center():
 
 
 def update_clusters():
-    for cluster_idx in range(DATA_COUNT):
-        distances = np.zeros(K)
-        for centroid_idx in range(K):
-            distances[centroid_idx] = calc_distance(dataset[cluster_idx], centroid_list[centroid_idx])
-        cluster_list[cluster_idx] = np.argmin(distances)
+    for data_idx in range(DATA_COUNT):
+        distances = [calc_distance(dataset[data_idx], centroid_list[idx]) for idx in range(K)]
+        cluster_list[data_idx] = np.argmin(distances)
 
 
 def update_centers():
     for idx in range(K):
         same_cluster = [dataset[cluster_idx] for cluster_idx in range(DATA_COUNT) if cluster_list[cluster_idx] == idx]
-        centroid_list[idx] = np.round(np.mean(same_cluster, axis=0), 3)
+        centroid_list[idx] = np.mean(same_cluster, axis=0)
 
 
 def calc_distance(data, center):  # data 와 center 간의 거리 구하기
-    dist_list = [(data - center) ** 2 for data, center in list(zip(data, center))]  # 12개 요소 각각의 차이
-    return np.round(sum(dist_list) ** 0.5, 3)
-    #  return np.round(sum([(data - center) ** 2 for data, center in list(zip(data, center))]) ** 0.5, 3)
+    return np.round(sum([(data - center) ** 2 for data, center in list(zip(data, center))]) ** 0.5, 3)
 
 
 def read_file(args):
