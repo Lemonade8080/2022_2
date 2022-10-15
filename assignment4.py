1e77047c40b6e1223ab9ff998097e5cc330f07ca
Lemonade8080 <58336426+Lemonade8080@users.noreply.github.com>
2022-10-16 05:53:19 +0900

    Hierachical Clustering

--- assignment4.py
+++ assignment4.py
@@ -3,82 +3,87 @@ import numpy as np
 import time
 import sys
 
-
 DATA_COUNT = 500  # GENE 개수
 dataset = []
 cluster = [i for i in range(DATA_COUNT)]
-# cluster = [4, 5, 9, 7, 4, 5, 6, 7, 9, 9]
-i_j_distance = np.zeros((DATA_COUNT, DATA_COUNT))
-
-
-def main():
-    read_file()
-    assignment4()
 
 
 def assignment4():
+    # single_link_distance()
+    complete_link_distance()
 
-    # start_time = time.process_time() * 1000
+
+def single_link_distance():
+    start_time = time.process_time() * 1000
     clustering1()
-    # end_time = time.process_time() * 1000
-    # single_time = end_time - start_time
+    end_time = time.process_time() * 1000
 
-    # start_time = time.process_time() * 1000
-    # clustering2()
-    # end_time = time.process_time() * 1000
-    # complete_time = start_time - end_time
+    write_file1(end_time - start_time)
+    print('time : ', end_time - start_time, 'ms')
 
-    # print('time : ', single_time, 'ms')
-    # print('time : ', complete_time, 'ms')
+
+def clustering1():
+    distance_data = []
+    for i in range(DATA_COUNT):
+        for j in range(i + 1, DATA_COUNT):
+            distance_data.append([calc_distance(i, j), i, j])
+
+    sorted_data = sorted(distance_data, key=lambda x: x[0])
+
+    count = 0
+    for data in sorted_data:
+        if data[0] > 20:
+            print(data[1], '와', data[2], '의 거리가 20을 초과하여 중단합니다')
+            break
+
+        if cluster[data[1]] != cluster[data[2]]:
+            merge_cluster([cluster[data[1]], cluster[data[2]]])
+
+    print('\nClustering Count: ', count, '\n')
+
+
+def complete_link_distance():
+    start_time = time.process_time() * 1000
+    clustering2()
+    end_time = time.process_time() * 1000
+
+    write_file2(end_time - start_time)
+    print('time : ', end_time - start_time, 'ms')
 
 
 def clustering2():
-    init_distance()
+    distance_data = []
+    for i in range(DATA_COUNT):
+        for j in range(i + 1, DATA_COUNT):
+            distance_data.append([calc_distance(i, j), i, j])
+
+    count = 0
 
     while True:
-        cluster_set = get_cluster_set()
-        cluster_len = len(cluster_set)
-        
-        if cluster_len == 1:
-            print('하나의 Cluster 로 합쳐짐')
+        min_idx = np.argmin([dist[0] for dist in distance_data])
+        min_pos = distance_data[min_idx][1:3]
+        min_dist = distance_data[min_idx][0]
+
+        if min_dist > 20:
+            print('20 초과')
             break
 
-        distance_data = []   # 거리, 인덱스1(cluster_list), 인덱스2(cluster_list)
-        for i in range(cluster_len):
-            for j in range(i + 1, cluster_len):
-                idx1 = cluster_set[i]
-                idx2 = cluster_set[j]
-                distance_data.append([complete_link(idx1, idx2), i, j])
+        merge_cluster(min_pos)
+        update_data = [data for data in distance_data if min_pos[0] not in data]
 
-        min_dists = [dist[0] for dist in distance_data]
-        data_idx = np.argmin(min_dists)
+        for data in update_data:
+            if min_pos[1] in data:
+                data = update(data, min_pos)
 
-        print('Min of Distance:', min(min_dists))
+        distance_data = update_data
 
-        if distance_data[data_idx][0] > 20:
-            print('20 초과하여 중단')
+        if len(distance_data) == 0:
+            print('클러스터 종료')
             break
 
-        set_idx1 = distance_data[data_idx][1]
-        set_idx2 = distance_data[data_idx][2]
-        cluster_idx1 = cluster_set[set_idx1]
-        cluster_idx2 = cluster_set[set_idx2]
-        print('Complete_Index:', cluster_idx1, cluster_idx2)
-        merge_cluster(cluster_idx1, cluster_idx2)
+        count += 1
 
-
-def complete_link(idx1, idx2):
-    cluster_idx1 = cluster[idx1]
-    cluster_idx2 = cluster[idx2]
-    cluster1 = [idx for idx in range(len(cluster)) if cluster[idx] == cluster_idx1]
-    cluster2 = [idx for idx in range(len(cluster)) if cluster[idx] == cluster_idx2]
-    # print('cluster2: ', cluster2)
-    # print('cluster1: ', cluster1)
-    distance = min([i_j_distance[idx1][idx2] for idx1 in cluster1 for idx2 in cluster2])
-    # distance2 = min([i_j_distance[idx1][idx2] for idx1 in cluster1 for idx2 in cluster2])
-    # print('max: ', distance)
-    # print('min: ', distance2)
-    return distance
+    print('\nClustering Count: ', count, '\n')
 
 
 def get_cluster_set():
@@ -87,40 +92,25 @@ def get_cluster_set():
     return index_list
 
 
-def init_distance():
-    for i in range(DATA_COUNT):  # 500개 간의 거리 값 설정
-        i_j_distance[i] = [calc_distance(i, j) for j in range(DATA_COUNT)]
+def merge_cluster(cluster_idx):
+    for idx in range(DATA_COUNT):
+        if cluster[idx] == cluster_idx[0]:
+            cluster[idx] = cluster_idx[1]
+    print('Move Cluster:', cluster_idx[0], 'to', cluster_idx[1])
+    print('Now  Cluster:', cluster, '\n')
 
 
-def clustering1():
-    count = 0
+def update(data, min_pos):
+    if data[1] == min_pos[1]:
+        if calc_distance(data[2], min_pos[0]) > data[0]:
+            data[0] = calc_distance(data[1], min_pos[0])
+            # print('Change Data:', data)
 
-    distance_data = []
-    for i in range(DATA_COUNT):
-        for j in range(i + 1, DATA_COUNT):
-            distance_data.append([calc_distance(i, j), i, j])
-
-    sorted_distances = sorted(distance_data, key=lambda x: x[0])
-    merge_list = [dist[1:3] for dist in sorted_distances]
-
-    for merge in merge_list[0:20]:
-        if calc_distance(merge[0], merge[1]) > 20:
-            print(merge[0], '와', merge[1], '의 거리가 20을 초과하여 중단합니다')
-            break
-
-        if cluster[merge[0]] != cluster[merge[1]]:
-            merge_cluster(cluster[merge[0]], cluster[merge[1]])
-            count += 1
-
-    print('\nClustering Count: ', count, '\n')
-
-
-def merge_cluster(idx1, idx2):
-    for i in range(DATA_COUNT):
-        if cluster[i] == idx1:
-            cluster[i] = idx2
-    print('Change Cluster:', idx1, 'to', idx2)
-    # print('After  Cluster:', cluster, '\n')
+    elif data[2] == min_pos[1]:
+        if calc_distance(data[1], min_pos[0]) > data[0]:
+            data[0] = calc_distance(data[1], min_pos[0])
+            # print('Change Data:', data)
+    return data
 
 
 def calc_distance(idx1, idx2):
@@ -136,15 +126,17 @@ def read_file():
     file.close()
 
 
-def write_file(work_time1, work_time2):
+def write_file1(work_time):
     with open("assignment4_output1.txt", "w") as file:
         file.write(get_result())
-        file.write('time : ' + str(work_time1) + 'ms')
+        file.write('time : ' + str(work_time) + 'ms')
     file.close()
 
+
+def write_file2(work_time):
     with open("assignment4_output2.txt", "w") as file:
         file.write(get_result())
-        file.write('time : ' + str(work_time2) + 'ms')
+        file.write('time : ' + str(work_time) + 'ms')
     file.close()
 
 
@@ -163,5 +155,6 @@ def get_result():
 
 
 if __name__ == '__main__':
-    # main(sys.argv[1])
-    main()
+    # read_file(sys.argv[1])
+    read_file()
+    assignment4()
